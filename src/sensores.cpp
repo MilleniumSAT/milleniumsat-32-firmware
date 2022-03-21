8c34840ce612570a11325b65fc6af65072ebac22
Antonio <antonio_pereira@outlook.com>
2022-03-21 20:48:25 -0300

    feat: incluindo funcionamento gps

--- src/sensores.cpp
+++ src/sensores.cpp
@@ -1,17 +1,33 @@
 #include "sensores.h"
 #include <ArduinoJson.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
 #include "MadgwickAHRS.h"
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
 StaticJsonDocument<500> pacote;
 const float sensorRate = 104.00;
 static Utils utils;
-static const int RXPin = 16, TXPin = 17;
 static const uint32_t GPSBaud = 9600;
-TinyGPSPlus gps;
-SoftwareSerial ss(RXPin, TXPin);
 Madgwick filter;
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(1);
+struct GpsDataState_t
+{
+  double originLat = 0;
+  double originLon = 0;
+  double originAlt = 0;
+  double distMax = 0;
+  double dist = 0;
+  double altMax = -999999;
+  double altMin = 999999;
+  double spdMax = 0;
+  double prevDist = 0;
+};
+GpsDataState_t gpsState = {};
+
+#define TASK_SERIAL_RATE 1000 // ms
+uint32_t nextSerialTaskTs = 0;
+uint32_t nextOledTaskTs = 0;
 
 String Sensores::obtemJSON()
 {
@@ -84,36 +100,88 @@ String Sensores::obtemJSON()
   pacote["v"] = V_FIRMWARE;
 
   utils.enviaMensagem("[SENSORS] JSON de envio montado com sucesso.", SERIAL_DEBUG, SEM_TOPICO);
-/*
-  ss.begin(9600, SWSERIAL_8N1, 13, 12, false);
-  if (!ss)
-  {
-    Serial.println("Invalid SoftwareSerial pin configuration, check config");
-  }
-  delay(1000);
-  while (ss.available() > 0)
-  {
-    Serial.println(".");
 
-    while (!gps.location.isValid())
+  SerialGPS.begin(9600, SERIAL_8N1, 16, 17);
+  long readValue;
+
+  gpsState.originLat = (double)readValue / 1000000;
+
+  gpsState.originLon = (double)readValue / 1000000;
+
+  gpsState.originAlt = (double)readValue / 1000000;
+
+  static int p0 = 0;
+  int numeroLeituras = 5000;
+while (1){
+  // GPS Koordinaten von Modul lesen
+  gpsState.originLat = gps.location.lat();
+  gpsState.originLon = gps.location.lng();
+  gpsState.originAlt = gps.altitude.meters();
+
+  gpsState.distMax = 0;
+  gpsState.altMax = -999999;
+  gpsState.spdMax = 0;
+  gpsState.altMin = 999999;
+
+  while (SerialGPS.available() > 0)
+  {
+    gps.encode(SerialGPS.read());
+  }
+
+  if (gps.satellites.value() > 4)
+  {
+    gpsState.dist = TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), gpsState.originLat, gpsState.originLon);
+
+    if (gpsState.dist > gpsState.distMax && abs(gpsState.prevDist - gpsState.dist) < 50)
     {
-      gps.encode(ss.read());
-      delay(100);
+      gpsState.distMax = gpsState.dist;
+    }
+    gpsState.prevDist = gpsState.dist;
+
+    if (gps.altitude.meters() > gpsState.altMax)
+    {
+      gpsState.altMax = gps.altitude.meters();
     }
 
-    double latitude = (gps.location.lat());
-    double longitude = (gps.location.lng());
-    Serial.print("Latitude: ");
-    Serial.println(latitude, 6);
-    Serial.print("Longitude: ");
-    Serial.println(longitude, 6);
+    if (gps.speed.kmph() > gpsState.spdMax)
+    {
+      gpsState.spdMax = gps.speed.kmph();
+    }
+
+    if (gps.altitude.meters() < gpsState.altMin)
+    {
+      gpsState.altMin = gps.altitude.meters();
+    }
   }
 
-  if (gps.charsProcessed() < 10)
+  if (nextSerialTaskTs < millis())
   {
-    Serial.println(F("No GPS detected: check wiring."));
+    Serial.print("LAT=");
+    float lat = gps.location.lat();
+    float lon = gps.location.lng();
+    float alt = gps.altitude.meters();
+    float spd = gps.speed.kmph();
+
+    Serial.print("LAT=");
+    Serial.print(lat);
+    Serial.print("LON=");
+    Serial.print(lon);
+    Serial.print("ALT=");
+    Serial.print(alt);
+    Serial.print("SPD=");
+    Serial.print(spd);
+
+    nextSerialTaskTs = millis() + TASK_SERIAL_RATE;
+
+    pacote["lat"] = lat;
+    pacote["lon"] = lon;
+    pacote["alt"] = alt;
+
+    if (lat != 0){
+      break;
+    }
   }
-*/
+}
   String conteudoEnvio = "";
   serializeJson(pacote, conteudoEnvio);
   Serial.println("[UTILS] Pacote: " + conteudoEnvio);
